b2bcd9d180692b2db39f3dd73d1ed1558bdc3f2d
LauryCGit <93162728+LauryCGit@users.noreply.github.com>
2024-04-08 21:16:30 -0300

    Estructura

---
+++ frontend/src/components/Caracteristicas/Caracteristicas.jsx
@@ -0,0 +1,13 @@
+import './caracteristicas.css';
+
+
+const Caracteristicas = () => {
+    return ( 
+        <div>
+            <h3>Titulo</h3>
+
+        </div>
+     );
+}
+ 
+export default Caracteristicas;
