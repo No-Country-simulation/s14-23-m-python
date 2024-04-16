f568f67131669d80601fee1894ba416d9511cf8d
madev7 <madazv7@gmail.com>
2024-04-16 11:17:31 -0500

    [add] Products application

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from apps.category.models import Category
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    size = models.CharField(max_length=50)
+    native_place = models.TextField(null=True)
+    location = models.TextField(null=True)
+    toxicity = models.TextField(null=True)
+    light_quantity = models.PositiveSmallIntegerField(null=True)
+    watering = models.PositiveSmallIntegerField(null=True)
+    additional_data = models.TextField(null=True)
+    price = models.FloatField(default=0.0)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    images_url = models.TextField()
+    created_at = models.DateTimeField()
